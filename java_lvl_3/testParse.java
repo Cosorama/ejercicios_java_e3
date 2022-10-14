699faa2166743c6225430f775ee51494676c74c1
Gerardo Andrés Luna Navarro <94817965+Cosorama@users.noreply.github.com>
2022-10-13 23:39:19 -0300

    [JAVA] Ejercicios nivel #3.
    Subida de ejercicios de nivel #3, sin uso de librerías innecesarias para comprender e integrar mejor la lógica del lenguaje.

---
+++ java_lvl_3/testParse.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class testParse {
+    public static void main(String[] args) {
+        LocalDate hora = LocalDate.parse("2018-12-27");
+
+        System.out.println(hora);
+    }
+}
